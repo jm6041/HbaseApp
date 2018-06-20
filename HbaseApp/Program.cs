2a406e7b5bf5e4b3ec17947b44ddf5dd7404a305
JM <jmmail@foxmail.com>
2018-06-20 17:07:41 +0800

    添加项目文件。

---
+++ HbaseApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HbaseApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
